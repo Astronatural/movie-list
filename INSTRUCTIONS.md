7bf030fe983b5df8cf0bfb0934826678b4135d4e
JDK <jdkranzler@yahoo.com>
2021-02-13 17:41:43 -0600

    planning routes

--- INSTRUCTIONS.md
+++ INSTRUCTIONS.md
@@ -46,7 +46,7 @@ X  This view is completed already! It displays all of the movies in the movie da
 
 X- TODO: When a movie poster is clicked, a user should be brought to the `/details` view for that movie. 
 
-- TODO: Have a way to get to the Add Movie Page
+X- TODO: Have a way to get to the Add Movie Page
 
 ### Details Page
 
--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,9 +1,30 @@
+//import { useSelector, useDispatch } from 'react-redux';
 
-function  MovieDetails(){
 
-    return(
-        <h2>Here are some details on that movie you clicked on.</h2>
+function MovieDetails() {
+    // the plan here is that we will have two GETs; one to get the movie details(by Id).
+    // and another to get the genres (by movie Id).
+    
+    // const dispatch = useDispatch();
+    // const reduxStore = useSelector(store => store);
 
+    // useEffect(() => {
+    //     dispatch({ type: 'FETCH_DETAILS' });
+    // }, []);
+
+    return (
+        <>
+            <h2>Here are some details on that movie you clicked on.</h2>
+            {/* {reduxStore.detailsReducer.map(details =>
+                <div key={details.id}>
+                    <h1>{details.title}</h1>
+                    <img src={details.url} />
+                    <p>{details.description}</p>
+                    <p>Genre(s): {details.genres} </p>
+                    <button onClick={nextLink}>Back to List</button>
+                </div>
+            )} */}
+        </>
     )
 };
 
